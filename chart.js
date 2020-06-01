cd285f7a267a3ec5484102bc8463032bdbbe50b0
adrianWadrzyk <adrian.wadrzyk.contact@gmail.com>
2020-06-01 10:28:53 +0200

    Create chart for country

--- chart.js
+++ chart.js
@@ -1,9 +1,60 @@
-new Chartist.Line('#chart', {
-  labels: [1, 2, 3, 4, 5, 6, 7, 8],
-  series: [
-    [5, 9, 7, 8, 5, 3, 5, 4]
-  ]
-}, {
-  low: 0,
-  showArea: true
-});
+
+var dane = [1,2,3,4,5,6,7,8]
+
+const convertData = (data) => { 
+  
+ const confirmed = [], active = [], days=[];
+
+  data.forEach(element => {
+    active.push(element.Active);
+    confirmed.push(element.Confirmed); 
+ });
+
+  for (let i=0; i < data.length; i++)
+        days[i] = i;
+
+
+    const config = { 
+      type: 'line', 
+      data: { 
+        labels: days, 
+        datasets: [{
+          label: 'Confirmed',
+          data: confirmed,
+          borderColor: 'red'
+        }, {
+          label: 'Active',
+          data: active,
+          borderColor: "blue",
+        }]
+      }, 
+      options: { 
+        responsive: true, 
+        title: { 
+          display: true, 
+          text: ' chart multiple'
+        }, 
+        scales:  {
+          yAxes: [{
+            ticks: { 
+              stepSize: 2500
+            }
+          }]
+        }
+      }
+    };
+
+    createChart(config);
+};
+
+
+const createChart = (config) => { 
+  var ctx = document.getElementById('chart').getContext('2d');
+  let chart = new Chart (ctx, config);
+}
+
+export default convertData;
+
+
+
+
--- staticforCountry.js
+++ staticforCountry.js
@@ -1,9 +1,12 @@
 
+import convertData from "./chart.js";
+
 async function getCountryData(country) {
     try {
       const response = await fetch(`https://api.covid19api.com/total/dayone/country/${country}`);
       const json = await response.json();
-      await showStaticsForCountry(json)
+      await showStaticsForCountry(json);
+      console.log(json);
     } catch (err) {
       console.log(err);
     }
@@ -13,9 +16,7 @@ const showStaticsForCountry  = (data) => {
     const lengthData = data.length-1;
     let last = data[lengthData];
     let oneDayBefore = data[lengthData-1];
-    console.log(last);
-    console.log(oneDayBefore);
-
+ 
     const total_deaths = document.querySelector(".stats--deaths");
     const total_infected = document.querySelector(".stats--infected");
     const total_recovered = document.querySelector(".stats--recovered");
@@ -24,6 +25,7 @@ const showStaticsForCountry  = (data) => {
     total_infected.innerText = `${last.Confirmed} (+ ${last.Confirmed - oneDayBefore.Confirmed})`;
     total_recovered.innerText = `${last.Recovered} (+ ${last.Recovered - oneDayBefore.Recovered})`;
   
+    convertData(data);
   }
 
   export default getCountryData;
